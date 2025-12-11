e62c72282186818e061cc7211aa680d6f0d2b91d
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-11 17:16:06 +0530

    Added routes for accept friendreq

--- backend/src/route/user.route.ts
+++ backend/src/route/user.route.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import {
+  acceptFriendRequest,
   getFriendList,
   getRecommendations,
+  sendFriendRequest,
 } from "../controller/user.controller.js";
 import protectRoute from "../middleware/auth.middleware.js";
 
@@ -9,5 +11,10 @@ const userRouter = express.Router();
 
 userRouter.use(protectRoute);
 userRouter.post("/", getRecommendations).post("/friends", getFriendList);
+
+userRouter
+  .route("/friend-req/:id")
+  .post(sendFriendRequest)
+  .put(acceptFriendRequest);
 
 export default userRouter;
