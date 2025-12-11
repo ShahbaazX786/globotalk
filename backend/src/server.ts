0a6caaad0c362168883aed9e179acb438b90edc3
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-11 16:36:45 +0530

    utilized cookieparser middleware.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import express from "express";
 import connectDB from "./lib/db.js";
@@ -12,6 +13,7 @@ app.get("/", (_req, res) => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, async () => {
