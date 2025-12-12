6ffdcece8e48ebd2158919f32e5c2d30b1cf749e
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-12 16:32:50 +0530

    added signup page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router";
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router";
 import LoginPage from "./pages/Auth/LoginPage";
 import OnboardingPage from "./pages/Auth/OnboardingPage";
 import SignUpPage from "./pages/Auth/SignUpPage";
@@ -8,15 +7,31 @@ import CallPage from "./pages/Communication/CallPage";
 import ChatPage from "./pages/Communication/ChatPage";
 import NotificationsPage from "./pages/Communication/NotificationsPage";
 import HomePage from "./pages/HomePage";
+import { useQuery } from "@tanstack/react-query";
+import API from "./lib/api";
 
 function App() {
+  const { data: authData } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await API.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+
+  const authUser = authData?.user;
+
   return (
     <div data-theme="cupcake" className="w-full h-screen">
       {/* <h1 className="text-3xl font-bold underline text-green-500">
         Bismillah!
       </h1> */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
--- frontend/src/pages/Auth/SignUpPage.tsx
+++ frontend/src/pages/Auth/SignUpPage.tsx
@@ -1,5 +1,41 @@
+import { ShipWheelIcon } from "lucide-react";
+import { useState } from "react";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <section
+      className="w-full h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="w-full max-w-5xl mx-auto bg-base-100 border border-primary/25 flex flex-col lg:flex-row rounded-xl shadow-lg overflow-hidden">
+        <section
+          id="left-section"
+          className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col"
+        >
+          <div className="mb-4 flex justify-start items-center gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="font-bold font-mono text-3xl bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              GloboTalk
+            </span>
+          </div>
+
+          <div className="w-full">
+            <form></form>
+          </div>
+        </section>
+        <section
+          id="right-section"
+          className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col"
+        ></section>
+      </div>
+    </section>
+  );
 };
 
 export default SignUpPage;
