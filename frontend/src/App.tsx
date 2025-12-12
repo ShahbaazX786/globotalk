98abcbcdec21732ce47c43a6a2664507f6e52e81
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-12 15:44:51 +0530

    installed and configured + tested daisyui.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div data-theme="cupcake" className="w-full h-screen">
       <h1 className="text-3xl font-bold underline text-green-500">
         Bismillah!
       </h1>
-    </>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
   );
 }
 
