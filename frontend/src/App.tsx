ae0e59eecbbbf7637b213d1e8038ea098d8a8e30
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-12 15:57:07 +0530

    Added dummy pages + routes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,28 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import LoginPage from "./pages/Auth/LoginPage";
+import OnboardingPage from "./pages/Auth/OnboardingPage";
+import SignUpPage from "./pages/Auth/SignUpPage";
+import CallPage from "./pages/Communication/CallPage";
+import ChatPage from "./pages/Communication/ChatPage";
+import NotificationsPage from "./pages/Communication/NotificationsPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div data-theme="cupcake" className="w-full h-screen">
-      <h1 className="text-3xl font-bold underline text-green-500">
+      {/* <h1 className="text-3xl font-bold underline text-green-500">
         Bismillah!
-      </h1>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+      </h1> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
     </div>
   );
 }
