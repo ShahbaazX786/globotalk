e58c00757474a0bea6706c034f1f2b644ced097c
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-13 10:28:21 +0530

    Adding cors to ensure security is enforced for proper origin + added origin in env + added cors types.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import express from "express";
 import connectDB from "./lib/db.js";
 import authRoutes from "./route/auth.route.js";
+import cors from "cors";
 
 config();
 const app = express();
@@ -12,6 +13,7 @@ app.get("/", (_req, res) => {
   res.send("Bismillah");
 });
 
+app.use(cors({ origin: process.env.ORIGIN!, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
