7e5cc5eec5a68a8f85368530c45f918444730996
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-14 17:56:38 +0530

    fixed chat route not being visible.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -83,7 +83,7 @@ function App() {
           }
         />
         <Route
-          path="/chat"
+          path="/chat/:id"
           element={
             isAuthenticated && isOnboarded ? (
               <Layout showSidebar={false}>
