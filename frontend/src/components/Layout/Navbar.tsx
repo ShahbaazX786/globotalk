4a049a1549e1e5f39d9c08ff2e3bb94621f89231
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-15 11:46:13 +0530

    Fixed Brand Name not being visible in header in other pages + alignment.

--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -1,13 +1,11 @@
 import { BellIcon, LogOutIcon, PhoneCall } from "lucide-react";
-import { Link, useLocation } from "react-router";
+import { Link } from "react-router";
 import useAuthUser from "../../lib/hooks/useAuthUser";
 import { useLogout } from "../../lib/hooks/useMutations";
 import ThemeSelector from "../Misc/ThemeSelector";
 
 const Navbar = () => {
   const { authUser } = useAuthUser();
-  const location = useLocation();
-  const isChatPage = location.pathname?.startsWith("/chat");
   const { logoutMutation } = useLogout();
 
   const handleLogout = () => {
@@ -16,18 +14,16 @@ const Navbar = () => {
 
   return (
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center justify-between">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto lg:px-4">
         <div className="flex justify-end items-center w-full gap-2">
-          {isChatPage && (
-            <div className="pl-5">
-              <Link to={"/"} className="flex items-center gap-2.5">
-                <PhoneCall className="size-8 text-primary" />
-                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-widerk">
-                  GloboTalk
-                </span>
-              </Link>
-            </div>
-          )}
+          <div className="lg:hidden">
+            <Link to={"/"} className="flex items-center gap-2.5">
+              <PhoneCall className="size-8 text-primary" />
+              <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-widerk">
+                GloboTalk
+              </span>
+            </Link>
+          </div>
 
           <div className="flex items-center gap-3 sm:gap-4 ml-auto">
             <Link to={"/notifications"}>
