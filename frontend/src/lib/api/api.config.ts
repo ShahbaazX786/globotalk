80dc871ffa5723f6a8f523af5ad8562b81e6da84
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-16 17:08:35 +0530

    Added new env variable to separate modes of running the app.

--- frontend/src/lib/api/api.config.ts
+++ frontend/src/lib/api/api.config.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5173/api/v1"
+    : "/api/v1";
+
 const API = axios.create({
-  baseURL: "http://localhost:7200/api/v1",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
