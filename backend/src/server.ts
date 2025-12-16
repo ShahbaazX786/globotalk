639863800223a2fc874d31c2c5ec22b932dc62a0
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-16 17:08:58 +0530

    Updated file path in server to serve static frontend files from backend.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,15 +2,18 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { config } from "dotenv";
 import express from "express";
+import path from "path";
 import connectDB from "./lib/db.js";
 import authRoutes from "./route/auth.route.js";
+import chatRoutes from "./route/chat.route.js";
 import userRoutes from "./route/user.route.js";
 
 config();
 const app = express();
 const PORT = process.env.PORT || 4000;
+const __dirname = path.resolve();
 
-app.get("/", (_req, res) => {
+app.get("/status", (_req, res) => {
   res.send("Bismillah");
 });
 
@@ -19,6 +22,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use((_req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, async () => {
   await connectDB();
